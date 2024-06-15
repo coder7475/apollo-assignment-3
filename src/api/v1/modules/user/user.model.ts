3d4334f8eba88f9196e4be4a48de80cef4453780
coder7475 <robiul7475@gmail.com>
2024-06-15 23:26:09 +0600

    feat: create user model

---
+++ src/api/v1/modules/user/user.model.ts
@@ -0,0 +1,32 @@
+import { Schema, model } from 'mongoose';
+import IUser from './user.interface';
+
+const userSchema = new Schema<IUser>({
+    name: {
+        type: String,
+    },
+    email: {
+        type: String,
+    },
+    password: {
+        type: String,
+    },
+    phone: {
+        type: String,
+    },
+    address: {
+        type: String,
+    },
+    role: {
+        type: String,
+        enum: ['admin', 'user'],
+    },
+    isDeleted: {
+        type: Boolean,
+        default: false,
+    },
+});
+
+const User = model<IUser>('User', userSchema);
+
+export default User;

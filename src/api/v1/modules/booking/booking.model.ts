83625116300195794e69850a33dfc64403151328
coder7475 <robiul7475@gmail.com>
2024-06-16 00:01:23 +0600

    feat: create booking model

---
+++ src/api/v1/modules/booking/booking.model.ts
@@ -0,0 +1,37 @@
+import { Schema, model } from 'mongoose';
+import IBooking from './booking.interface';
+
+const bookingSchema = new Schema({
+    userId: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+        unique: true,
+    },
+    bikeId: {
+        type: Schema.Types.ObjectId,
+        ref: 'Bike',
+        required: true,
+        unique: true,
+    },
+    startTime: {
+        type: Date,
+        required: true,
+    },
+    returnTime: {
+        type: Date,
+        default: null,
+    },
+    totalCost: {
+        type: Number,
+        default: 0,
+    },
+    isReturned: {
+        type: Boolean,
+        default: false,
+    },
+});
+
+const Booking = model<IBooking>('Booking', bookingSchema);
+
+export default Booking;

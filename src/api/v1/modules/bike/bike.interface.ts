d537ee26367be74a963cafbefa9dce9de3850976
coder7475 <robiul7475@gmail.com>
2024-06-15 23:33:47 +0600

    feat: create bike interface

---
+++ src/api/v1/modules/bike/bike.interface.ts
@@ -0,0 +1,12 @@
+export interface IBike {
+    name: string;
+    description: string;
+    pricePerHour: number;
+    cc: number;
+    year: number;
+    model: string;
+    brand: string;
+    isAvailable: boolean;
+}
+
+export default IBike;

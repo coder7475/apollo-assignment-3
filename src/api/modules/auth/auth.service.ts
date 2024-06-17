f0994062f6e8d7bfc2d546264cf66875ce383483
coder7475 <robiul7475@gmail.com>
2024-06-17 22:11:30 +0600

    feat: write down the todos for logging in user

--- src/api/modules/auth/auth.service.ts
+++ src/api/modules/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import IUser from '../user/user.interface';
 import User from '../user/user.model';
+import ILogin from './auth.interface';
 
 /**
  * It asynchronously creates a new user using the provided user data.
@@ -12,8 +13,18 @@ const createNewUser = async (userData: IUser) => {
     return result.toObject();
 };
 
+const loginUser = async (loginData: ILogin) => {
+    // TODO 1: check if user exists
+    // TODO 2: check if user is already deleted
+    // TODO 3: check if password is correct
+    // TODO 4: create jwt token
+    // TODO 5: return the token
+    return;
+};
+
 const AuthServices = {
     createNewUser,
+    loginUser,
 };
 
 export default AuthServices;

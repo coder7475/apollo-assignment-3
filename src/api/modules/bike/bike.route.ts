110eaa4138495eeac0205120eeaf589a861d1fbe
coder7475 <robiulhossain7475@gmail.com>
2024-06-20 17:45:21 +0600

    feat: validate request by schema

--- src/api/modules/bike/bike.route.ts
+++ src/api/modules/bike/bike.route.ts
@@ -2,18 +2,30 @@
 import { Router } from 'express';
 import BikeController from './bike.controller';
 import auth from '../../../middlewares/auth';
+import validateRequest from '../../../middlewares/validateRequest';
+import zodBikeSchema, { zodPartialBikeSchema } from './bike.validation';
 
 const router = Router();
 
 // * api/bikes
 // admin can create new bike
-router.post('/', auth('admin'), BikeController.createBike);
+router.post(
+    '/',
+    auth('admin'),
+    validateRequest(zodBikeSchema),
+    BikeController.createBike,
+);
 
 // get all bikes
 router.get('/', BikeController.allBikes);
 
 // update a bike
-router.put('/:id', auth('admin'), BikeController.updateBike);
+router.put(
+    '/:id',
+    auth('admin'),
+    validateRequest(zodPartialBikeSchema),
+    BikeController.updateBike,
+);
 
 // delete a bike
 router.delete('/:id', auth('admin'), BikeController.deleteBike);

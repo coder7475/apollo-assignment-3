0003d093a25d53e57e3aca7644c400358dd37782
coder7475 <robiulhossain7475@gmail.com>
2024-06-20 12:35:05 +0600

    feat: delete some useless comments in user controller

--- src/api/modules/user/user.controller.ts
+++ src/api/modules/user/user.controller.ts
@@ -7,13 +7,8 @@ import UserServices from './user.service';
 import AppError from '../../../errors/AppError';
 
 const getProfile = catchAsync(async (req, res) => {
-    // console.log(req.headers.cookie?.split(';')[1].split('=')[1]);
-    // console.log(req.headers.authorization);
     const token = req.headers.authorization?.split(' ')[1];
     // const cookies = req.cookies;
-    // if (!cookies.accessToken) {
-    //     throw new AppError(httpStatus.FORBIDDEN, 'You are not authorized');
-    // }
     if (!token) {
         throw new AppError(httpStatus.FORBIDDEN, 'You are not authorized');
     }

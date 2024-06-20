c2729985247d814af0c32d9445900be2d081276a
coder7475 <robiulhossain7475@gmail.com>
2024-06-20 12:16:15 +0600

    feat: validate access token

--- src/api/modules/user/user.controller.ts
+++ src/api/modules/user/user.controller.ts
@@ -7,18 +7,21 @@ import AppError from '../../../errors/AppError';
 const getProfile = catchAsync(async (req, res) => {
     // console.log(req.headers.cookie?.split(';')[1].split('=')[1]);
     // console.log(req.headers.authorization);
-    // const token = req.headers.authorization?.split(' ')[1];
-    const cookies = req.cookies;
-    if (!cookies.accessToken) {
+    const token = req.headers.authorization?.split(' ')[1];
+    // const cookies = req.cookies;
+    // if (!cookies.accessToken) {
+    //     throw new AppError(httpStatus.FORBIDDEN, 'You are not authorized');
+    // }
+    if (!token) {
         throw new AppError(httpStatus.FORBIDDEN, 'You are not authorized');
     }
-    const user = UserServices.getUserProfile(cookies);
-
+    const user = await UserServices.getUserProfile(token as string);
+    console.log(user);
     sendResponse(res, {
         statusCode: httpStatus.OK,
         success: true,
         message: 'User profile retrieved successfully',
-        data: cookies,
+        data: user,
     });
 });
 
--- src/api/modules/user/user.service.ts
+++ src/api/modules/user/user.service.ts
@@ -1,5 +1,17 @@
-const getUserProfile = async (cookies: object) => {
-    console.log(cookies);
+import jwt, { JwtPayload } from 'jsonwebtoken';
+import parameters from '../../../parameters';
+
+const getUserProfile = async (token: string) => {
+    // checking if the given token is valid
+    const decoded = jwt.verify(
+        token,
+        parameters.secret as string,
+    ) as JwtPayload;
+    const { email, role, iat, exp } = decoded;
+
+    return {
+        token,
+    };
 };
 
 const UserServices = {
